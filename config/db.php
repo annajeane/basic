1d0ce2f52aa1215e16ead70fa5e1cea7d7337059
brandon-tanujaya <39830148+brandon-tanujaya@users.noreply.github.com>
2018-11-05 14:31:41 +0700

    cleanup
    db name -> ti_umn
    removed .sql

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=basic',
+    'dsn' => 'mysql:host=localhost;dbname=ti_umn',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
